04dfef01b4cae8ba46b6e5a6a816565457d251e9
Shikaga <shikaga@gmail.com>
2012-09-23 18:46:27 +0100

    The application now generates snowflakes across the whole screen

--- js/main.js
+++ js/main.js
@@ -6,17 +6,35 @@ function add(number1, number2)
 
 // Creates canvas 320 × 200 at 10, 50
 var paper = Raphael(0, 0, "100%", "100%");
+var svg = document.getElementsByTagName("svg")[0];
+console.log(svg);
+svg.setAttribute("pointer-events", "none");
 
-// Creates circle at x = 50, y = 40, with radius 10
-var circle = paper.circle(50, 40, 10);
-var circle2 = paper.circle(90, 80, 10);
-// Sets the fill attribute of the circle to red (#f00)
-circle.attr("fill", "#fff");
-circle2.attr("fill", "#fff");
+var SnowflakeGenerator = function() {
+}
+
+SnowflakeGenerator.prototype.createSnowflake = function(x, y)
+{
+    var circle = paper.circle(x, y, 5);
+    circle.attr("fill", "#fff");
+    var anim = Raphael.animation({cx: x, cy: 500}, 2e3);
+    circle.animate(anim); // run the given animation immediately
+}
+
+SnowflakeGenerator.prototype.generateSnowflakes = function()
+{
+    var self = this;
+    setInterval(function() {
+	var widthOffsetRatio = Math.random();
+	self.createSnowflake(document.width*widthOffsetRatio,40);
+    }, 20);
+}
+
+var snowflakeGenerator = new SnowflakeGenerator();
+snowflakeGenerator.generateSnowflakes();
 
 
-var anim = Raphael.animation({cx: 50, cy: 500}, 2e3);
-circle.animate(anim); // run the given animation immediately
-//circle.animate(anim.delay(500));
 
-//document.body.appendChild(circle);
+
+
+//pointer-events="none"
