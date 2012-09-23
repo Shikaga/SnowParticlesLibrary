35125214b4efb383389aaa0f894d0d181987b8e4
Shikaga <shikaga@gmail.com>
2012-09-23 15:47:40 +0100

    So this is the basic idea

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@ function add(number1, number2)
 {
 	return number1 + number2;
 }
+
+
+// Creates canvas 320 × 200 at 10, 50
+var paper = Raphael(0, 0, "100%", "100%");
+
+// Creates circle at x = 50, y = 40, with radius 10
+var circle = paper.circle(50, 40, 10);
+var circle2 = paper.circle(90, 80, 10);
+// Sets the fill attribute of the circle to red (#f00)
+circle.attr("fill", "#fff");
+circle2.attr("fill", "#fff");
+
+
+var anim = Raphael.animation({cx: 50, cy: 500}, 2e3);
+circle.animate(anim); // run the given animation immediately
+//circle.animate(anim.delay(500));
+
+//document.body.appendChild(circle);
