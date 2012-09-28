8d95af8e5f5dfeff595a0cbc6613860d8c862d38
Shikaga <shikaga@gmail.com>
2012-09-28 20:56:30 +0100

    The Tests file is now much cleaner and understandable

--- test/tests.js
+++ test/tests.js
@@ -1,66 +1,77 @@
-test( "test-snowflake-start-position-range", function() {
+function testOffset(sie, timeToFall, expectedOffset)
+{
+    test("offset", function() {
+	var offset = sie.getStartPositionXOfSnowflake(timeToFall);
+	equal(expectedOffset, offset);
+    });
+}
+
+function testRange(sie, timeToFall, expectedRange)
+{
+    test("range", function() {
+	var range = sie.getRange(timeToFall);
+	equal(expectedRange, range);
+    });
+} 
+
+function testWindDrift(sie, timeToFall, expectedOffset)
+{
+    test("wind drift", function() {
+	var offset = sie.getXEndOffset(timeToFall);
+	equal(expectedOffset, offset);
+    });
+} 
+
 
     //Setup
     var wind = 0.2; // 200 pixels to the right per second
     var bottomOfScreen = 2000;
     var timeToFallToBottomOfScreen = 10000; //10 Seconds
     var widthOfScreen = 1000;
-    var sig = new SnowflakeInitializationEngine(wind, bottomOfScreen, widthOfScreen);
-    //Offset test
+    var windToRightSIE = new SnowflakeInitializationEngine(wind, bottomOfScreen, widthOfScreen);
     var expectedXStartPosition = -2000; //pixels to the left of the screen
-    var actualXStartPosition = sig.getStartPositionXOfSnowflake(timeToFallToBottomOfScreen);
-    ok( expectedXStartPosition == actualXStartPosition, "Passed!" );
+    testOffset(windToRightSIE, timeToFallToBottomOfScreen, expectedXStartPosition);
 
     //Range test
     var expectedStartWidthRange = widthOfScreen - expectedXStartPosition
-    var actualStartWidthRange = sig.getRange(timeToFallToBottomOfScreen);
-    ok(expectedStartWidthRange == actualStartWidthRange, "Passed!");
+
+    testRange(windToRightSIE, timeToFallToBottomOfScreen, expectedStartWidthRange);
 
     var expectedXEndOffset = wind * timeToFallToBottomOfScreen;
-    var actualXEndOffset = sig.getXEndOffset(timeToFallToBottomOfScreen);
-});
+    testWindDrift(windToRightSIE, timeToFallToBottomOfScreen, expectedXEndOffset);
 
-test( "test-snowflake-start-position-range", function() {
 
     //Setup
     var wind = -0.2; // 200 pixels to the right per second
     var bottomOfScreen = 2000;
     var timeToFallToBottomOfScreen = 10000; //10 Seconds
     var widthOfScreen = 1000;
-    var sig = new SnowflakeInitializationEngine(wind, bottomOfScreen, widthOfScreen);
+    var windToLeftSIE = new SnowflakeInitializationEngine(wind, bottomOfScreen, widthOfScreen);
     //Offset test
     var expectedXStartPosition = 0; //pixels to the left of the screen
-    var actualXStartPosition = sig.getStartPositionXOfSnowflake(timeToFallToBottomOfScreen);
-    ok( expectedXStartPosition == actualXStartPosition, "Passed!" );
+    testOffset(windToLeftSIE, timeToFallToBottomOfScreen, expectedXStartPosition);
 
     //Range test
     var expectedStartWidthRange = widthOfScreen + 2000
-    var actualStartWidthRange = sig.getRange(timeToFallToBottomOfScreen);
-    ok(expectedStartWidthRange == actualStartWidthRange, "Passed!");
+    testRange(windToLeftSIE, timeToFallToBottomOfScreen, expectedStartWidthRange);
 
     var expectedXEndOffset = wind * timeToFallToBottomOfScreen;
-    var actualXEndOffset = sig.getXEndOffset(timeToFallToBottomOfScreen);
-});
-
-test( "test-snowflake-start-position-range", function() {
+    testWindDrift(windToLeftSIE, timeToFallToBottomOfScreen, expectedXEndOffset);
 
     //Setup
     var wind = 0; // 200 pixels to the right per second
     var bottomOfScreen = 2000;
     var timeToFallToBottomOfScreen = 10000; //10 Seconds
     var widthOfScreen = 1000;
-    var sig = new SnowflakeInitializationEngine(wind, bottomOfScreen, widthOfScreen);
+    var noWindSIE = new SnowflakeInitializationEngine(wind, bottomOfScreen, widthOfScreen);
     //Offset test
     var expectedXStartPosition = 0; //pixels to the left of the screen
-    var actualXStartPosition = sig.getStartPositionXOfSnowflake(timeToFallToBottomOfScreen);
-    ok( expectedXStartPosition == actualXStartPosition, "Passed!" );
+    testOffset(noWindSIE, timeToFallToBottomOfScreen, expectedXStartPosition);
 
     //Range test
     var expectedStartWidthRange = widthOfScreen - expectedXStartPosition
-    var actualStartWidthRange = sig.getRange(timeToFallToBottomOfScreen);
-    ok(expectedStartWidthRange == actualStartWidthRange, "Passed!");
+    testRange(noWindSIE, timeToFallToBottomOfScreen, expectedStartWidthRange);
 
     var expectedXEndOffset = wind * timeToFallToBottomOfScreen;
-    var actualXEndOffset = sig.getXEndOffset(timeToFallToBottomOfScreen);
-});
+    testWindDrift(noWindSIE, timeToFallToBottomOfScreen, expectedXEndOffset);
 
